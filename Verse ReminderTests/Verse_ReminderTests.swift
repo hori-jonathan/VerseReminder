082c41521288078968e25238cd6d1dd3ba2571d9
Jonathan Hori <jonathanhori@gmail.com>
2025-07-08 23:12:10 -0400

    Move git repo root to parent directory

---
+++ Verse ReminderTests/Verse_ReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  Verse_ReminderTests.swift
+//  Verse ReminderTests
+//
+//  Created by Jonathan Hori on 7/1/25.
+//
+
+import Testing
+@testable import Verse_Reminder
+
+struct Verse_ReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
